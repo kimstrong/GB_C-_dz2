64ba75be295943597f248533fd4ca215b3e0f57a
kimst <kimstrong@mail.ru>
2022-09-04 15:08:11 +1100

    first commit

---
+++ Sem_2_dz2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number/100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    Console.WriteLine(number%10);
+};
+
+
---
+++ Sem_2_dz3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 8)
+
+{
+    if (number == 1 ^ number == 2 ^ number == 3 ^ number == 4 ^ number == 5)
+    {
+        Console.WriteLine("Этот день не выходной");
+    }
+    if (number == 6 ^ number == 7)
+    {
+        Console.WriteLine("Это выходной");
+    }
+}
+
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+};
